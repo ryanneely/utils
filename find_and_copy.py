18d59d1b4a7688ccedf604aa77d3d749c5369411
ryanneely <ryan_m_neely@protonmail.com>
2019-12-03 09:16:07 -0800

    First commit

---
+++ find_and_copy.py
@@ -0,0 +1,10 @@
+##find_and_copy.py
+
+##a function to look through a directory of files, find a file
+##of a certain type, and if it exists, copy it to a new
+#common location
+
+##by Ryan Neely 12/3/19
+
+import os
+import shutil
